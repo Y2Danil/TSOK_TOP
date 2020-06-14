62dbc07abb4085ba621438555205629d857ed241
Y2Danil <mc_danil@inbox.ru>
2020-06-14 18:04:48 +1200

    Лень что-то писать

--- Top/models.py
+++ Top/models.py
@@ -1,3 +1,4 @@
+import datetime
 from django.db import models
 from django.conf import settings
 from django.utils import timezone
@@ -8,13 +9,16 @@ class Article(models.Model):
   create_author = models.CharField('Автор идеи', max_length=100)
   title = models.CharField('Название статьи', max_length=200)
   text = models.TextField('Текст статьие', blank=True, null=True)
-  created_date = models.DateTimeField(default=timezone.now, db_index=True)
-  published_date = models.DateTimeField(blank=True, null=True, db_index=True)
+  created_date = models.DateTimeField(db_index=True, auto_now_add=True)
+  published_date = models.DateTimeField(blank=True, null=True, db_index=True, auto_now_add=True)
 
   class Meta:
     verbose_name = 'Статья'
     verbose_name_plural = 'Статьи'
     ordering = ['-published_date']
+    
+  def new_article(self):
+    return self.published_date >= (timezone.now() - datetime.timedelta(days = 7))
 
   def publish(self):
     self.published_date = timezone.now()
@@ -27,7 +31,7 @@ class Comment(models.Model):
   article = models.ForeignKey(Article, on_delete=models.CASCADE)
   author = models.CharField('Автор комментария', max_length=60)
   comment_text = models.TextField('Текст комметария', max_length=500)
-  pub_date = models.DateTimeField('Дата публикации', default=timezone.now, db_index=True)
+  pub_date = models.DateTimeField('Дата публикации', db_index=True, auto_now_add=True)
 
   class Meta:
     verbose_name = 'Комментарий'
